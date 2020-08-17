259bae49035cacac40b350f75da91f09ff1857e7
Sam Bonias <sbonias84@gmail.com>
2020-08-16 21:19:33 -0400

    coded footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faSquare } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
-
+    <footer className="footer">
+      <div className="container"></div>
+      {/* <!-- github link --> */}
+      <a href="https://github.com/sbonias/" target="_blank">
+        <span className="text-white">
+          Github &nbsp;
+          <i className="text-white fa fa-github fa-lg"></i>
+        </span>
+      </a>
+      {/* <!-- divider --> */}
+      <span className="text-white"> &nbsp;|&nbsp; </span>
+      {/* <!-- linkedin link --> */}
+      <a
+        href="https://www.linkedin.com/in/sam-bonias-064934194"
+        target="_blank"
+      >
+        <span className="text-white">
+          Linked &nbsp;
+          <FontAwesomeIcon icon={[fab]} />
+          <i className="text-white fa fa-linkedin fa-md"></i>
+        </span>
+      </a>
+      {/* <!-- divider --> */}
+      <span className="text-white"> &nbsp;|&nbsp; </span>
+      {/* <!-- resume link --> */}
+      <a
+        href="https://docs.google.com/document/d/160xYP5v_EW6ak_HxVfU16ciec7NTnsB6qZyULshmMm0/edit?usp=sharing"
+        target="_blank"
+      >
+        <span className="text-white">
+          Resume &nbsp;
+          <i className="text-white fa fa-book fa-md"></i>
+          <FontAwesomeIcon icon={faBook} />
+        </span>
+      </a>
+      {/* </div> */}
+    </footer>
   );
 }
 
