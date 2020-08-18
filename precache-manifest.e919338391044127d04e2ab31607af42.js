a89f393ceb11f06fe753030788617407d1b178be
Sam Bonias <sbonias84@gmail.com>
2020-08-18 10:56:25 -0400

    Updates

---
+++ precache-manifest.e919338391044127d04e2ab31607af42.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd1e76a3b78a96774afcfeed4c0efd4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "70cb1f7cb607f619feed",
+    "url": "/react-portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d2a05b535c69067df3e1",
+    "url": "/react-portfolio/static/css/main.6c9fdc15.chunk.css"
+  },
+  {
+    "revision": "70cb1f7cb607f619feed",
+    "url": "/react-portfolio/static/js/2.48e48834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.48e48834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a05b535c69067df3e1",
+    "url": "/react-portfolio/static/js/main.7c4676a3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "ae513d19a406593c0d3b293f876a22ab",
+    "url": "/react-portfolio/static/media/GrandCanyon.ae513d19.jpeg"
+  },
+  {
+    "revision": "ff3aa8f9c1ad22a82a2947bf0e6b078e",
+    "url": "/react-portfolio/static/media/GrandCanyon_2.ff3aa8f9.jpeg"
+  },
+  {
+    "revision": "e586ae794e267aaabe6c233ccb73a591",
+    "url": "/react-portfolio/static/media/code.e586ae79.jpg"
+  },
+  {
+    "revision": "bde89f9089f5fa7762331e4b4a924f5d",
+    "url": "/react-portfolio/static/media/earth.bde89f90.jpg"
+  },
+  {
+    "revision": "dad11484b2859a9134409cc22fb682ed",
+    "url": "/react-portfolio/static/media/reading.dad11484.jpg"
+  },
+  {
+    "revision": "1f9fc9e5edc47a146343d7263f0afa4d",
+    "url": "/react-portfolio/static/media/virus.1f9fc9e5.jpg"
+  },
+  {
+    "revision": "33081129509ae48558c1d4be5eb76999",
+    "url": "/react-portfolio/static/media/wallpaper.33081129.jpg"
+  }
+]);
